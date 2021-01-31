bff72c578dd9e15cbb99440aeab6211edb994f4e
Andrii <andriigromakov@gmail.com>
2021-01-31 14:45:57 +0200

    update main.js: app draw new items and save their values

---
+++ main.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".add-item-form");
+const list = document.querySelector(".list")
+
+const state = [];
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const text = e.target.item.value;
+  const item = {
+    text,
+    id: Date.now(),
+    complete: false
+  };
+  state.push(item);
+  e.target.reset();
+  list.dispatchEvent(new CustomEvent('updateApp'));
+}
+function drawList () {
+  const html = state.map((elem) => `
+  <li>
+  <input type="checkbox" value="${elem.id}">
+  <span>${elem.text}</span>
+  <button value="${elem.id}">&times;</button>
+  </li>
+  `).join('');
+  list.innerHTML = html;
+}
+
+form.addEventListener('submit', handleSubmit);
+list.addEventListener('updateApp', drawList);
